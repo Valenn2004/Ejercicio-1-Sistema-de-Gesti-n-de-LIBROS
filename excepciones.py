f5c063a391b001145c44644da0c42d3d4cc8c5e9
Valentin <45506187@terciariourquiza.edu.ar>
2026-05-25 20:41:21 -0300

    Datos Actualizados

---
+++ excepciones.py
@@ -0,0 +1,23 @@
+class LibroNoEncontrado(Exception):
+    pass
+
+class MiembroNoEncontrado(Exception):
+    pass
+
+class LibroNoDisponible(Exception):
+    pass
+
+class LibroYaDisponible(Exception):
+    pass
+
+class ISBNInvalido(Exception):
+    pass
+
+class DNIInvalido(Exception):
+    pass
+
+class LibroDuplicado(Exception):
+    pass
+
+class MiembroDuplicado(Exception):
+    pass
